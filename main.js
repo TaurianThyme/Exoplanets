d129a993562063ec8f18894080156e42ace04dfb
TaurianThyme <85608816+TaurianThyme@users.noreply.github.com>
2022-07-29 18:57:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function video() {
+    window.location.href = 'https://www.youtube.com/watch?v=k1UcseLVNVc'
+}
+
+function website() {
+    window.location.href = 'https://exoplanets.nasa.gov/what-is-an-exoplanet/overview/.'
+}
